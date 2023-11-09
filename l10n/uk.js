cdba97e97ea7ed278f741f8d26614aad335783ba
Magnus Anderssen <magnus@magooweb.com>
2023-11-09 17:46:25 +0100

    chore(l10n): uk small change

--- l10n/uk.js
+++ l10n/uk.js
@@ -1,22 +1,26 @@
 OC.L10N.register(
-    "thesearchpage",
-    {
-    "The Search Page" : "Сторінка пошуку",
-    "Provides a proper search page" : "Надає результати пошуку",
-    "Provides a search page to your Nextcloud instance." : "Виводить результати пошуку у хмарі Nextcloud на окремій сторінці.",
-    "All providers" : "Всюди",
-    "Search Page" : "Сторінка пошуку",
-    "There was an error loading the providers." : "Помилка під час завантаження постачальників даних.",
-    "See only results for this provider" : "Показати результати пошуку лише від цього постачальника",
-    "Show only" : "Перейти",
-    "See all providers" : "Показати розширені результати пошуку",
-    "Back" : "Назад",
-    "Loading..." : "Завантаження...",
-    "No results" : "Нічого не знайдено",
-    "Load more..." : "Більше...",
-    "Clear current query" : "Очистити поточний запит",
-    "Search" : "Пошук",
-    "Click to change providers" : "Натисніть, щоб змінити постачальника",
-    "Filters" : "Фільтри"
-},
-"");
+  "thesearchpage",
+  {
+    "The Search Page": "Сторінка пошуку",
+    "Provides a proper search page": "Надає результати пошуку",
+    "Provides a search page to your Nextcloud instance.":
+      "Виводить результати пошуку у хмарі Nextcloud на окремій сторінці.",
+    "All providers": "Все",
+    "Search Page": "Сторінка пошуку",
+    "There was an error loading the providers.":
+      "Помилка під час завантаження постачальників даних.",
+    "See only results for this provider":
+      "Показати результати пошуку лише від цього постачальника",
+    "Show only": "Перейти",
+    "See all providers": "Показати розширені результати пошуку",
+    Back: "Назад",
+    "Loading...": "Завантаження...",
+    "No results": "Нічого не знайдено",
+    "Load more...": "Більше...",
+    "Clear current query": "Очистити поточний запит",
+    Search: "Пошук",
+    "Click to change providers": "Натисніть, щоб змінити постачальника",
+    Filters: "Фільтри",
+  },
+  ""
+);
